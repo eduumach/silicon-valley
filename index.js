2eda0b24d42914469fe3cf3f5c4883b7aa97ddae
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2025-06-04 01:19:43 -0300

    Implement responsive navigation menu and integrate JavaScript for menu toggle functionality

--- index.js
+++ index.js
@@ -7,24 +7,44 @@ let big = document.querySelector(".imb_big")
 let minis = document.querySelectorAll(".imb_mini")
 
 minis.forEach(item => {
-    item.addEventListener("click", () => {
-        big.src = item.src
-    })
+  item.addEventListener("click", () => {
+    big.src = item.src
+  })
 })
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.toggle('active', i === index);
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM pronto");
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
+  console.log("toggle:", toggle);
+  console.log("menu:", menu);
+
+  if (!toggle || !menu) {
+    console.error("Elemento(s) não encontrado(s)!");
+    return;
+  }
+
+  toggle.addEventListener("click", () => {
+    console.log("Botão clicado");
+    menu.classList.toggle("show");
   });
-}
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
+  }
 
-prevBtn.addEventListener('click', () => {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
+  prevBtn.addEventListener('click', () => {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  });
+
+  nextBtn.addEventListener('click', () => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  });
+
+
 });
 
-nextBtn.addEventListener('click', () => {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-});
 
 
